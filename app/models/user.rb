36b8785fe6625b51e7689198e9c2545ddfaa63c7
joshualamai <joshualamai2004@gmail.comgit config --global user.email joshualamai2004@gmail.com>
2024-12-22 16:46:08 +0000

    Project Complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :logs, dependent: :destroy 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
